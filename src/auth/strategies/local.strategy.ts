89727b2b78f95abbbd790b9f0fd4a93684bf158f
baranozdemir1 <info@baranozdemir.com>
2024-10-20 20:25:14 +0300

    refactor(local.strategy): change validate method

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,31 +1,18 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
-  constructor(private readonly authService: AuthService) {
+  constructor(private authService: AuthService) {
     super({
       usernameField: 'email',
+      passwordField: 'password',
     });
   }
 
   async validate(email: string, password: string) {
-    // try {
-    //   LoginSchema.parse({ email, password });
-    // } catch (error) {
-    //   console.log('LocalStrategy.validate.error', error);
-    //   if (error instanceof ZodError) {
-    //     throw new BadRequestException({
-    //       message: error.errors.map((err) => err.message),
-    //       error: 'Bad Request',
-    //       statusCode: 400,
-    //     });
-    //   }
-    //   throw error;
-    // }
-
-    return this.authService.verify(email, password);
+    return this.authService.validateUser(email, password);
   }
 }
