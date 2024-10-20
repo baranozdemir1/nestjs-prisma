9a8d8a854a5d612a5e83a9148eee2e278b2001fa
baranozdemir1 <info@baranozdemir.com>
2024-10-20 20:30:06 +0300

    refactor(user.controller): add findAll endpoint for get all users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,27 +9,25 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { Prisma, User } from '@prisma/client';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { CurrentUser } from '../auth/current-user.decorator';
+import { Prisma } from '@prisma/client';
 import { RegisterDto } from '../auth/dto/register.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async findAll() {
+    return this.userService.findAll();
+  }
 
   @Post()
   create(@Body() createUserDto: RegisterDto) {
     return this.userService.create(createUserDto);
   }
 
-  @Get()
   @UseGuards(JwtAuthGuard)
-  async findAll(@CurrentUser() user: User) {
-    console.log(user);
-    return this.userService.findAll();
-  }
-
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
