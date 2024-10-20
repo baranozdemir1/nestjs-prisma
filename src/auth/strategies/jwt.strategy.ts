54de874a6d15c76e918c26e320a07de48876a9f9
baranozdemir1 <info@baranozdemir.com>
2024-10-20 20:28:05 +0300

    fix(jwt.strategy): fix JwtStrategy import error

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,19 +1,18 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-local';
-import { ExtractJwt } from 'passport-jwt';
-import { Request } from 'express';
+import { Strategy, ExtractJwt } from 'passport-jwt';
 import { TokenPayload } from '../../interface/token-payload.interface';
 import { UserService } from '../../user/user.service';
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly userService: UserService) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) => request.cookies?.Authentication,
+        (req: Request) => req.cookies?.Authentication,
       ]),
-      secretOrKey: process.env.JWT_ACCESS_TOKEN_SECRET,
+      secretOrKey: process.env.JWT_ACCESS_TOKEN_SECRET || 'secret',
     });
   }
 
