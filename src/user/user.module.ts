e4fb44e6d5643ba2fe41cfba0c193e56546d5f5b
baranozdemir1 <info@baranozdemir.com>
2024-10-21 00:35:42 +0300

    refactor(user.module): integrate MailerService as a provider

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { DatabaseModule } from '../database/database.module';
+import { MailerService } from '../mailer/mailer.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, MailerService],
   exports: [UserService],
 })
 export class UserModule {}
