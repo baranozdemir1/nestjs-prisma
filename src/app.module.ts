901fb36729e045ab08102a34dffd1e8fa56b71db
baranozdemir1 <info@baranozdemir.com>
2024-10-21 00:34:09 +0300

    refactor(app.module): integrate MailerService as a provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { DatabaseModule } from './database/database.module';
 import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { MailerService } from './mailer/mailer.service';
 
 @Module({
   imports: [DatabaseModule, ProductModule, AuthModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MailerService],
 })
 export class AppModule {}
