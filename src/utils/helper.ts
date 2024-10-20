a108373ad5416be9a1d1e3557099f3c9d28fb210
baranozdemir1 <info@baranozdemir.com>
2024-10-21 00:33:22 +0300

    feat(utils/helper): implement Helper class

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,22 +1,28 @@
-export const escapeHtml = (text: string) => {
-  const map: Record<string, string> = {
-    '<': '&lt;',
-    '>': '&gt;',
-    '&': '&amp;',
-    "'": '&#39;',
-    '"': '&quot;',
-    '/': '&#47;',
-    '%': '&#37;',
-    '+': '&#43;',
+export class Helper {
+  static generateVerificationToken = () => {
+    return Math.floor(100000 + Math.random() * 900000).toString();
   };
-  return text.replace(/[<>&'"\/]/g, (char: string): string => map[char]);
-};
 
-export const exclude = <T extends object, Key extends keyof T>(
-  obj: T,
-  keys: Key[],
-): Omit<T, Key> => {
-  return Object.fromEntries(
-    Object.entries(obj).filter(([key]) => !keys.includes(key as Key)),
-  ) as Omit<T, Key>;
-};
+  static exclude = <T extends object, Key extends keyof T>(
+    obj: T,
+    keys: Key[],
+  ): Omit<T, Key> => {
+    return Object.fromEntries(
+      Object.entries(obj).filter(([key]) => !keys.includes(key as Key)),
+    ) as Omit<T, Key>;
+  };
+
+  static escapeHtml = (text: string) => {
+    const map: Record<string, string> = {
+      '<': '&lt;',
+      '>': '&gt;',
+      '&': '&amp;',
+      "'": '&#39;',
+      '"': '&quot;',
+      '/': '&#47;',
+      '%': '&#37;',
+      '+': '&#43;',
+    };
+    return text.replace(/[<>&'"\/]/g, (char: string): string => map[char]);
+  };
+}
